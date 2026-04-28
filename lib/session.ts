e563493deca87d013b841c2d541c83f8217c9cf8
Itta Funahashi <itta@itta.dev>
2026-04-28 17:26:04 +0900

    chore: requireServerSessionを削除

--- lib/session.ts
+++ lib/session.ts
@@ -1,5 +1,4 @@
 import { headers } from "next/headers"
-import { redirect } from "next/navigation"
 
 import { auth } from "@/lib/auth"
 
@@ -7,14 +6,4 @@ export async function getServerSession() {
   return auth.api.getSession({
     headers: new Headers(await headers()),
   })
-}
-
-export async function requireServerSession() {
-  const session = await getServerSession()
-
-  if (!session) {
-    redirect("/")
-  }
-
-  return session
 }
