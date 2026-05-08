f15a42aa16151f7a3ac7bcd5f22fed79a0c22848
Itta Funahashi <itta@itta.dev>
2026-05-08 17:43:31 +0900

    fix: envv errror

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,19 @@
-import { env } from "@/lib/env"
 import { loadEnvConfig } from "@next/env"
 import { defineConfig } from "drizzle-kit"
 
 loadEnvConfig(process.cwd())
+
+const databaseUrl = process.env.DATABASE_URL
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is required")
+}
 
 export default defineConfig({
   schema: "./packages/db/schema/index.ts",
   out: "./packages/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: databaseUrl,
   },
 })
