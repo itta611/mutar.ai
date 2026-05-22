b87ec98d90c03e3020fba324ff3331abc366bb02
Itta Funahashi <itta@itta.dev>
2026-05-22 21:17:25 +0900

    feat: navバーUI

--- apps/web/components/navbar/index.tsx
+++ apps/web/components/navbar/index.tsx
@@ -1,34 +1,36 @@
 "use client"
 
-import { CopyIcon, PencilIcon, SaveIcon, XIcon } from "lucide-react"
+import { DownloadIcon, PencilLine, XIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 
 export function Navbar() {
   const router = useRouter()
+  const projectName = "プロジェクト名"
 
   return (
-    <nav className="shrink-0 flex h-13 items-center justify-between bg-sidebar border-b border-border/70 px-4">
+    <nav className="shrink-0 flex h-13 items-center bg-sidebar border-b border-border/70 pr-4 pl-2">
       <Button
         aria-label="ホームに戻る"
         onClick={() => router.push("/home")}
-        size="icon"
+        size="icon-lg"
         type="button"
         variant="ghost"
       >
         <XIcon />
       </Button>
+      <div className="grow pl-1">{projectName}</div>
       <div className="flex flex-row-reverse items-center gap-2">
         <Button type="button">
-          <SaveIcon />
-          保存
+          <DownloadIcon />
+          画像を保存
         </Button>
-        <Button aria-label="コピー" size="icon" type="button" variant="outline">
-          <CopyIcon />
+        <Button aria-label="コピー" type="button" variant="outline">
+          コピー
         </Button>
         <Button type="button" variant="outline">
-          <PencilIcon />
+          <PencilLine />
           修正
         </Button>
       </div>
