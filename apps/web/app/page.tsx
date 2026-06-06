28e52c092bd109be0ecf378b843234faae2d80f8
Itta Funahashi <itta@itta.dev>
2026-06-06 17:16:14 +0900

    style: lp

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import LogoIcon from "@/components/logo-icon"
 import { Button } from "@/components/ui/button"
 import { useAuthDialog } from "@/hooks/use-auth-dialog"
 
 export default function Page() {
   const { openAuthDialog } = useAuthDialog()
   return (
-    <div className="flex min-h-dvh w-full items-center justify-center">
-      <Button onClick={openAuthDialog}>Login</Button>
+    <div className="flex flex-col space-y-8 min-h-dvh w-full items-center justify-center">
+      <LogoIcon className="saturate-0 opacity-30 size-12" />
+      <Button onClick={openAuthDialog}>Hengenにログイン</Button>
     </div>
   )
 }
