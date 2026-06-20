7a7acb5046fe23862905ad8dc0a34b2a289fac5e
Itta Funahashi <itta@itta.dev>
2026-06-20 22:50:03 +0900

    feat: 入力つきカラーピッカーではHexを表示しない

--- apps/web/components/ui/color-picker.tsx
+++ apps/web/components/ui/color-picker.tsx
@@ -68,16 +68,47 @@ function ColorPicker({
   )
 }
 
-function ColorPickerWithInput(props: Parameters<typeof ColorPicker>[0]) {
+function ColorPickerWithInput({
+  id,
+  onBlur,
+  onValueChange,
+  value,
+}: Parameters<typeof ColorPicker>[0]) {
   return (
-    <div className="flex items-center gap-1.75 w-27 bg-zinc-100 rounded-md p-2 h-9">
-      <ColorPicker {...props} />
+    <div className="flex items-center gap-1.75 w-27 bg-zinc-100 dark:bg-zinc-800 rounded-md p-2 h-9">
+      <DropdownMenu
+        onOpenChange={(open) => {
+          if (!open) {
+            onBlur?.()
+          }
+        }}
+      >
+        <DropdownMenuTrigger
+          render={
+            <button
+              aria-label="カラー"
+              className="border size-5! rounded-xs"
+              style={{ backgroundColor: value }}
+              type="button"
+            />
+          }
+        />
+        <DropdownMenuContent
+          className={cn(
+            String.raw`space-y-3 w-auto p-3 bg-popover [&_.react-colorful]:h-auto! [&_.react-colorful\_\_pointer]:border-5! [&_.react-colorful\_\_pointer]:size-5!`,
+            String.raw`[&_.react-colorful\_\_saturation]:border-b-0! [&_.react-colorful\_\_saturation]:rounded-lg! [&_.react-colorful\_\_saturation]:h-50!`,
+            String.raw`[&_.react-colorful\_\_hue]:rounded-full! [&_.react-colorful\_\_hue]:mt-2! [&_.react-colorful\_\_hue]:border-x-12 [&_.react-colorful\_\_hue]:border-[#ff0000]`
+          )}
+        >
+          <HexColorPicker onChange={onValueChange} color={value} />
+        </DropdownMenuContent>
+      </DropdownMenu>
       <HexColorInput
         className="text-sm grow shrink basis-0 w-0 outline-0 font-mono uppercase"
-        color={props.value}
-        id={props.id}
-        onChange={props.onValueChange}
-        onBlur={props.onBlur}
+        color={value}
+        id={id}
+        onChange={onValueChange}
+        onBlur={onBlur}
       />
     </div>
   )
