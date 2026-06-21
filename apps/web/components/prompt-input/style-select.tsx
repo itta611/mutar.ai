016c5cdf24deb1c6f48bc967b3f8c8ebe43aa6cf
Itta Funahashi <itta@itta.dev>
2026-06-21 22:55:26 +0900

    feat: テクスチャ選択UI

--- apps/web/components/prompt-input/style-select.tsx
+++ apps/web/components/prompt-input/style-select.tsx
@@ -3,6 +3,44 @@ import { Button } from "../ui/button"
 import { ColorPickerWithInput } from "../ui/color-picker"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { Switch } from "../ui/switch"
+import Image from "next/image"
+import { cn } from "@/lib/utils"
+
+function MenuItem({
+  children,
+  onClick,
+  imageSrc,
+  selected = false,
+}: {
+  children: React.ReactNode
+  onClick?: () => void
+  imageSrc?: string
+  selected?: boolean
+}) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        `cursor-pointer rounded-lg grow basis-0 p-2 text-center text-xs text-muted-foreground`,
+        selected ? "border-2 border-primary" : "border"
+      )}
+    >
+      {imageSrc ? (
+        <div className="bg-muted w-full aspect-square rounded-md flex items-center justify-center mb-2">
+          <Image
+            src={imageSrc}
+            className="mx-auto"
+            alt=""
+            width={26}
+            height={26}
+          />
+        </div>
+      ) : null}
+      {children}
+    </button>
+  )
+}
 
 export type PromptStyle = {
   themeColor?: string
@@ -27,7 +65,15 @@ export function StyleSelect({
           </Button>
         }
       />
-      <PopoverContent align="start" className="min-w-68 p-4 gap-3">
+      <PopoverContent align="start" className="min-w-90 p-4 gap-3">
+        <span className="text-sm text-muted-foreground">テクスチャ</span>
+        <div className="grid grid-cols-3 gap-3 pb-1">
+          <MenuItem selected>選択しない</MenuItem>
+
+          <MenuItem imageSrc="/knight-flat.png">フラット</MenuItem>
+          <MenuItem imageSrc="/knight-gradient.png">立体感</MenuItem>
+          <MenuItem imageSrc="/knight-realistic.png">リアル</MenuItem>
+        </div>
         <div className="flex items-center justify-between h-9">
           <label
             className="text-sm text-muted-foreground"
